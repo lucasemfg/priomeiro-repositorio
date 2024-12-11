87f7810c6284e27bf0ff641fe884a9b4461d0f15
lucasemfg <lucasemfg@gmail.com>
2024-12-10 21:13:58 -0300

    soma

---
+++ 03.py
@@ -0,0 +1,9 @@
+n1 = int (input ('Digite um número:'))
+n2 = int (input ('Digite mais um número:'))
+soma = n1+n2
+print('A soma vale {}'.format(soma))
+
+n1 = float (input ('Digite um número:'))
+n2 = float (input ('Digite mais um número:'))
+soma = n1+n2
+print('A soma vale {}'.format(soma))
